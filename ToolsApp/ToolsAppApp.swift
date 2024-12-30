ebdab47c01bd71206140d95d420cd005e3dfa452
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-12-30 20:33:09 +0300

    Initial Commit

---
+++ ToolsApp/ToolsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolsAppApp.swift
+//  ToolsApp
+//
+//  Created by Роман on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToolsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
